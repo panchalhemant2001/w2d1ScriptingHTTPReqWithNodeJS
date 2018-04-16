7d8ea4af279c9ce39962a6df05ef4d5bae9811d4
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-04-16 21:07:04 +0000

    step 2 completed

--- main1.js
+++ main1.js
@@ -1,27 +1,29 @@
 var https = require('https');
 
 
-function getAndPrintHTMLChunks() {
-
+function getAndPrintHTML() {
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
+
+  var result = "";
 
   https.get(requestOptions, function(response) {
     response.setEncoding('utf8');
 
     response.on('data', function(data) {
       //console.log('Chunk Received. Length:', data.length);
-      console.log(data + "\n");
+      result += data;
     });
 
 
     response.on('end', function() {
+      console.log(result);
       console.log('Response stream complete');
     })
   });
-
 }
 
-getAndPrintHTMLChunks();
+
+getAndPrintHTML();
