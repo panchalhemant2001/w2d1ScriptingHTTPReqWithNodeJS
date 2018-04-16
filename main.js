75689c63b00af3bccf7ba7868728e0169e40fc84
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-04-16 20:56:04 +0000

    Step 1 completed

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var https = require('https');
+
+
 function getAndPrintHTMLChunks() {
 
   var requestOptions = {
@@ -5,6 +8,20 @@ function getAndPrintHTMLChunks() {
     path: 'http-examples/step1.html'
   };
 
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
 
+    response.on('data', function(data) {
+      //console.log('Chunk Received. Length:', data.length);
+      console.log(data + "\n");
+    });
+
+
+    response.on('end', function() {
+      console.log('Response stream complete');
+    })
+  });
 
 }
+
+getAndPrintHTMLChunks();
